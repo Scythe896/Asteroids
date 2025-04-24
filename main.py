15a8d302af835e528b926269839f7be3b4dfab9c
scythe896 <scythe096@gmail.com>
2025-04-24 15:23:53 +1000

    FPS Lesson

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from constants import *
 
 def main():
     pygame.init()
+
+    #Initial Messages
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #Initial Variables
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Creates black screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -16,6 +24,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
