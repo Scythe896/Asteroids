89efd8a61e8315970071340942549f463fa6418a
scythe896 <scythe096@gmail.com>
2025-04-26 13:41:53 +1000

    Ch2 L8: Groups

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+    #Groups setup
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #Initial Messages
@@ -22,9 +27,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
